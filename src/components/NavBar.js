15146911365e3ef3ab0c4a0208f319016dab5e4e
Vibudh08 <vibudhrathore8@gmail.com>
2023-08-05 20:09:29 +0530

    hamburger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './NavBar.css'
 // import blob from 'C:/Users/vibud/OneDrive/Desktop/WaveProj/my-app/src/Images/blob-top-left.png'
 import blob from '../Images/blob-top-left.png'
+// import { useState } from 'react'
 import bloob from '../Images/blob-top-right.png'
 import Ham from '../Images/Icons/hamburger.png'
+import X from '../Images/Icons/close.png'
+
 
 export default function NavBar() {
+    const [isMobile,setisMobile] =useState(false)
     return (
         <header className='navbar'>
         <img id="image1" src={blob}/>
@@ -13,7 +17,9 @@ export default function NavBar() {
             <div className='logoName'>
                 <h1>DGital</h1>
             </div>
-            <ul className='navul'>
+            <ul className={isMobile ? 'navul-mobile' : 'navul'}
+                onClick={()=>{setisMobile(true)}}
+            >
                 <li className='navli'><a href="#">Home</a></li>
                 <li className='navli'><a href="#">About</a></li>
                 <li className='navli'><a href="#">Service</a></li>
@@ -22,9 +28,11 @@ export default function NavBar() {
                 <li className='navli'><a href="#">Contact</a></li>
             </ul>
             <button className='btn'>Get Started</button>
-            <div className='trigger'>
-                <img src={Ham} alt="image" />
-            </div>
+            <button className='trigger'
+                onClick={()=>{setisMobile(!isMobile)}}
+            >
+                <img src={Ham} alt="" />
+            </button>
         </div>
         <img id="image2" src={bloob}/>
     </header>
