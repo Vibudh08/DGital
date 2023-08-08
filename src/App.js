738efb46390f119ab25b5be0ba097c7629abca2f
Vibudh08 <vibudhrathore8@gmail.com>
2023-08-09 01:23:28 +0530

    0908 links

--- src/App.js
+++ src/App.js
@@ -5,19 +5,42 @@ import Services from './components/Services';
 import Gallery from './components/Gallery'
 import Footer from './components/Footer';
 import Form from './components/Form';
-// import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
-      <About/>
-      <Services/>
-      <Gallery/>
-      <Form/>
-      <Footer/>
+      {/* <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path="/home" index element={<Banner/>} />  
+          <Route path="/about" element={<About/>} />  
+          <Route path="/service" element={<Services/>} />  
+          <Route path="/project" element={<Gallery/>} />  
+          <Route path="/contact" element={<Form/>} /> 
+        </Routes>
+        <Banner/>
+        <About/>
+        <Services/>
+        <Gallery/>
+        <Form/>
+        <Footer/>
+      </BrowserRouter> */}
+
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Banner />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Services />} />
+          <Route path="/project" element={<Gallery />} />
+          <Route path="/contact" element={<Form />} />
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,17 +18,15 @@ export default function NavBar() {
                 <h1>DGital</h1>
             </div>
             <ul className={isMobile ? 'navul-mobile' : 'navul'}
-                onClick={()=>{setiSMobile(false)}}
-            >
-                <li className='navli'><a href="/">Home</a></li>
-                <li className='navli'><a href="/about">About</a></li>
-                <li className='navli'><a href="/service">Service</a></li>
-                <li className='navli'><a href="project">Project</a></li>
-                <li className='navli'><a href="/pages">Pages +</a></li>
-                <li className='navli'><a href="/contact">Contact</a></li>
+                onClick={()=>{setiSMobile(false)}}>
+                <li className='navli'><Link to="/">Home</Link></li>
+                <li className='navli'><Link to="/about">About</Link></li>
+                <li className='navli'><Link to="/service">Service</Link></li>
+                <li className='navli'><Link to="/project">Project</Link></li>
+                <li className='navli'><Link to="/contact">Contact</Link></li>
             </ul>
             <img id="image2" src={bloob}/>
-            <button className='btn'>Get Started</button>
+            {/* <button className='btn'>Get Started</button> */}
             <button className='trigger'
                 onClick={()=>setiSMobile(!isMobile)}
             >
