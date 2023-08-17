08bbf1ad04323f8f7eec71e8bb836e4574276443
Vibudh08 <vibudhrathore8@gmail.com>
2023-08-17 21:55:25 +0530

    .

--- src/Pages/ServicesDetail.js
+++ src/Pages/ServicesDetail.js
@@ -21,14 +21,18 @@ export default function ServicesDetail() {
   console.log(categories)
   return (
     <div className='serviceDetails'>
+    <h1>Service Details</h1>
       <div className='serviceDetails-box'>
         {
           categories&&categories.map((items, index) => (
             <div className='content-box' key={index}>
+
+              {/* <h4>ID : {items.id}</h4>   */}
               <div className='content-img'>
                 <img src={items.imageUrl} alt="image" />
               </div>
               <h3>{items.name}</h3>
+              {/* <h4>Order By : {items.orderBy}</h4> */}
             </div>
           ))
         }
